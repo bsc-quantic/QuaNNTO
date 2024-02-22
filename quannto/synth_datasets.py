bd61ddf9f67a880f739259889dc7ae8d26f49620
Todorbsc <todor.krasimirov@bsc.es>
2024-02-22 18:54:44 +0100

    Add dataset to dataframe saving in csv format

--- quannto/synth_datasets.py
+++ quannto/synth_datasets.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 def test_function_1in_1out(inputs):
     return 0.7*inputs[0]**3 + 1.7*inputs[0]**2 + 2.4*inputs[0] + 5
@@ -10,8 +11,8 @@ def test_function_3in_1out(inputs):
     return 0.45*inputs[2] + 2.5*inputs[0]**2 + 1.2*inputs[2]*inputs[1]**2 + 0.5*inputs[0]*inputs[1] + 3
 
 def test_function_2in_2out(inputs):
-    return (0.7*inputs[0]**3 + 1.7*inputs[0]**2 + 2.4*inputs[0] + 5, 
-            0.2*inputs[1]**5 + 0.8*inputs[1]**3 + 1.1*inputs[1]**2 + 2)
+    return (0.25*inputs[1]**4 + 0.7*inputs[0]**3 + 1.7*inputs[1]**2 + 2.4*inputs[0] + 5, 
+            0.2*inputs[0]**5 + 0.8*inputs[1]**3 + 1.1*inputs[0]**2 + 2)
 
 def log_function_1in_1out(inputs):
     return 2 + 5*np.log(inputs[0])/inputs[0]
@@ -49,6 +50,13 @@ def generate_linear_dataset_of(target_function, num_inputs, num_outputs, num_sam
     
     return [inputs, outputs]
 
+def save_dataset_to_df(inputs_set, outputs_set, filename):
+    dataset = np.concatenate((inputs_set, outputs_set), axis=1)
+    cols = ["INPUT "+str(i+1) for i in range(len(inputs_set[0]))] + ["OUTPUT "+str(i+1) for i in range(len(inputs_set[0]))]
+    df = pd.DataFrame(dataset, columns=cols)
+    df.to_csv(filename+".csv", index=False)
+    return df
+
 def bubblesort(inputs, outputs):
     array = np.copy(outputs)
     inp = np.copy(inputs)
@@ -68,7 +76,6 @@ def replicate_inputs(inputs_set, n_inputs):
     for sample in range(len(inputs_set)):
         rep_inps[sample] = np.array(list(inputs_set[sample])*n_inputs)
     return rep_inps
- 
         
 def print_dataset(inputs, outputs):
     print('\n')
