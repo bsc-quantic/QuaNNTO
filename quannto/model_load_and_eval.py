2d76e0bd7330428fe9fbb302b9ef17f5999588ca
Todorbsc <todor.krasimirov@bsc.es>
2024-02-22 18:55:12 +0100

    Include model loading and evaluation module with arguments

---
+++ quannto/model_load_and_eval.py
@@ -0,0 +1,24 @@
+import argparse
+import pandas as pd
+import numpy as np
+from qnn import test_model, load_model
+from results_utils import plot_qnn_testing
+from synth_datasets import print_dataset
+
+parser = argparse.ArgumentParser(description="Load and evaluate a QNN model", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+parser.add_argument("file", help="QNN model file")
+parser.add_argument("dataset", help="Dataset to be evaluated")
+parser.add_argument("inputs", help="Number of inputs of the dataset")
+
+args = parser.parse_args()
+
+qnn_model = load_model(args.file)
+eval_df = pd.read_csv(args.dataset)
+n_in = int(args.inputs)
+
+inputs_set = eval_df.iloc[:, 0:n_in].to_numpy()
+outputs_set = eval_df.iloc[:, n_in:].to_numpy()
+print_dataset(inputs_set, outputs_set)
+
+qnn_test_outputs = test_model(qnn_model, [inputs_set, outputs_set])
+plot_qnn_testing(qnn_model, outputs_set, qnn_test_outputs)
