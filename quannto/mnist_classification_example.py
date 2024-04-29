94f8088f4813e6c3bc8a61dd36c37acd46d6aca2
Todorbsc <todor.krasimirov@bsc.es>
2024-04-29 16:56:14 +0200

    Modify hyperparameters and data processors

--- quannto/mnist_classification_example.py
+++ quannto/mnist_classification_example.py
@@ -6,23 +6,23 @@ from .results_utils import *
 from .preprocessing import *
 
 # === HYPERPARAMETERS DEFINITION ===
-N = 5
-layers = 2
+N = 6
+layers = 1
 observable_modes = [[0,0]]
 observable_types = [[1,0]]
 is_input_reupload = True
-n_inputs = 5
+n_inputs = 6
 n_outputs = 1
 
 num_categories = 10
-dataset_size = 120
+dataset_size = 150
 output_range = (0, 9)
-in_norm_range = (0.25, 4.75)
-out_norm_range = (0.51, 9.49)
+in_norm_range = (0.15, 6)
+out_norm_range = (1, 10)
 
 model_name = "mnist_encoded"
 testing_set_size = 100
-dataset = autoencode_mnist_four_latent_dim()
+dataset = autoencoder_mnist(N)
 
 # === PREPROCESSORS AND POSTPROCESSORS ===
 in_preprocessors = []
@@ -36,16 +36,24 @@ out_preprocessors.append(partial(rescale_data, data_range=output_range, scale_da
 postprocessors = []
 #postprocessors.append(partial(rescale_data, data_range=out_norm_range, scale_data_range=output_range))
 #postprocessors.append(partial(binning, data_range=out_norm_range, num_categories=num_categories))
-#postprocessors.append(partial(np.round))
-postprocessors.append(partial(np.floor))
+postprocessors.append(partial(np.round))
+postprocessors.append(partial(lambda x: x-1))
+#postprocessors.append(partial(np.floor))
 
 # === BUILD, TRAIN AND TEST QNN ===
 train_dataset = (dataset[0][:dataset_size], dataset[1][:dataset_size])
 
 # Build the QNN and train it with the generated dataset
-qnn = build_and_train_model(model_name, N, layers, n_inputs, n_outputs, observable_modes, observable_types, 
+qnn, loss = build_and_train_model(model_name, N, layers, n_inputs, n_outputs, observable_modes, observable_types, 
                             is_input_reupload, train_dataset, in_preprocessors, out_preprocessors, postprocessors)
 
+plt.plot(np.log(np.array(loss)+1), label=f'N={N}')
+plt.ylim(bottom=0.0)
+plt.xlabel('Epochs')
+plt.ylabel('Logarithmic loss value')
+plt.title(f'LOGARITHMIC LOSS FUNCTIONS')
+plt.legend()
+plt.show()
 # Generate a linearly-spaced testing dataset of the target function and test the trained QNN
 test_dataset = (dataset[0][dataset_size : dataset_size+testing_set_size], dataset[1][dataset_size : dataset_size+testing_set_size])
 qnn_test_outputs = test_model(qnn, test_dataset)
