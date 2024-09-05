f617424f9e16b7193736d49bb97f5e900f0ae1f2
Todorbsc <todor.krasimirov@bsc.es>
2024-09-05 11:32:15 +0200

    Add cross-entropy loss function

--- quannto/loss_functions.py
+++ quannto/loss_functions.py
@@ -1,4 +1,5 @@
 import numpy as np
+from .data_processors import softmax_discretization
 
 def mse(expected, obtained):
     return ((expected - obtained)**2).sum() / len(obtained)
@@ -6,8 +7,14 @@ def mse(expected, obtained):
 def nll(expected, obtained):
     return
 
+def cross_entropy(true_labels, obtained_outputs):
+    obtained_probs = softmax_discretization(obtained_outputs)
+    return (-np.sum(true_labels * np.log(obtained_probs))) / len(obtained_outputs)
+
 def retrieve_loss_function(loss_name):
     if loss_name == 'mse':
         return mse
     elif loss_name == 'nll':
         return nll
+    elif loss_name == 'cross_entropy':
+        return cross_entropy
