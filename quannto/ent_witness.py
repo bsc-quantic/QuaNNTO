f21498b461297484a993781ad786b0a607d5bd70
Todorbsc <todor.krasimirov@bsc.es>
2025-06-13 14:41:58 +0200

    Add parameters initialization

--- quannto/ent_witness.py
+++ quannto/ent_witness.py
@@ -13,7 +13,7 @@ from .loss_functions import *
 # === HYPERPARAMETERS DEFINITION ===
 modes = [2]
 photon_additions = [[0]]
-layers = [1]
+layers = [3]
 is_input_reupload = False
 n_inputs = 1
 n_outputs = 1
@@ -21,7 +21,15 @@ observable = 'witness'
 in_norm_range = (-2, 2)
 out_norm_range = (1, 5)
 loss_function = exp_val
-basinhopping_iters = 0 
+basinhopping_iters = 0
+params = np.array([
+ 0.82730684, 0.43208538, 0.18228398, 0.2870262,  0.197006,   0.81632739,
+ 0.75076105, 0.2439437,  0.52731704, 0.95077704, 0.18119154, 0.99306807,
+ 0.99667332, 0.6984572,  0.10448931, 0.82614215, 0.01884125, 0.02328073,
+ 0.5700898 , 0.87741522, 0.37338494, 0.6235469,  0.10093182, 0.46114707,
+ 0.68896096, 0.85563143, 0.84310635, 0.93252304, 0.25616017, 0.97075845,
+ 0.0897363 ])
+params = None
 
 # === BUILD, TRAIN AND TEST QNN MODELS WITH DIFFERENT MODES ===
 #colors = plt.cm.rainbow(np.linspace(0, 1, len(modes)))
@@ -35,7 +43,7 @@ for N in modes:
         for ph_add in photon_additions:
             model_name = "ent_witness"
             # Build the QNN and train it with the generated dataset
-            qnn, train_loss = train_entanglement_witness(model_name, N, l, n_inputs, n_outputs, ph_add, observable)
+            qnn, train_loss = train_entanglement_witness(model_name, N, l, n_inputs, n_outputs, ph_add, observable, init_pars=params)
             qnns.append(qnn)
             train_losses.append(train_loss.copy())
             qnn_loss.append(train_loss[-1])
