50c39f67c8084769c705a21f206f1d2a84f199bc
Todorbsc <todor.krasimirov@bsc.es>
2025-12-05 18:11:44 +0100

    Clean code

--- quannto/catstates_training.py
+++ quannto/catstates_training.py
@@ -12,7 +12,7 @@ np.random.seed(42)
 
 # === HYPERPARAMETERS DEFINITION ===
 modes = [2,2,2]
-photon_additions = [[[0]],[[0,0]],[[0,0,0]]]
+photon_additions = [[[0]], [[0,0]], [[0,0,0]]]
 layers = [1,1,1]
 is_addition = False
 include_initial_squeezing = False
@@ -21,15 +21,15 @@ is_passive_gaussian = False
 n_inputs = 1
 n_outputs = 1
 observable = 'catstates'
-in_norm_ranges = [(-1, 1)]*len(modes)
+in_norm_ranges = [(-2, 2)]*len(modes)
 out_norm_ranges = [(-2, 2)]*len(modes)
-loss_function = mse_energy_penalty
-basinhopping_iters = 1
+loss_function = mse
+basinhopping_iters = 5
 params = None
 
 # === DATASET SETTINGS ===
 phi = 0.0
-dataset_size = 50
+dataset_size = 1
 input_range = (-1, 1)
 alpha_list = np.linspace(input_range[0], input_range[1], dataset_size)
 model_name = f'catstate_phi{phi}_trainsize{dataset_size}_rng{alpha_list[0]}to{alpha_list[-1]}'
@@ -39,29 +39,11 @@ with open(f"datasets/catstate_phi{phi}_trainsize{dataset_size}_rng{input_range[0
     inputs = np.load(f)
 with open(f"datasets/catstate_phi{phi}_trainsize{dataset_size}_rng{input_range[0]}to{input_range[-1]}_outputs.npy", "rb") as f:
     outputs = np.load(f)
-train_dataset = [inputs, outputs]
 
-colors = colormaps['tab10']
-#expvals = ['⟨x⟩', '⟨p⟩', '⟨x²⟩', '⟨p²⟩', '⟨xp⟩', '⟨x³⟩', '⟨p³⟩', '⟨xp²⟩', '⟨x²p⟩']
-expvals = ["a²","n","n²"]
-start_expval = 1
-end_expval = 3
-c=0
-for exp_val_idx in range(start_expval, end_expval):
-    plt.plot(inputs, np.real_if_close(outputs[:,exp_val_idx]), c=colors(c), label=expvals[exp_val_idx])
-    c += 1
-plt.title(f'Dataset')
-plt.xlabel('x')
-plt.xlim()
-plt.ylabel('Statistical moments')
-plt.grid(linestyle='--', linewidth=0.4)
-plt.legend()
-plt.savefig("figures/trainset_"+model_name+".png")
-plt.show()
-plt.clf()
+train_dataset = [np.array(inputs), np.array(outputs)]
+
 
 # === BUILD, TRAIN AND TEST QNN MODELS WITH DIFFERENT MODES ===
-#colors = plt.cm.rainbow(np.linspace(0, 1, len(modes)))
 colors = colormaps['tab10']
 train_losses = []
 valid_losses = []
@@ -73,15 +55,11 @@ for (N, l, ph_add, in_norm_range, out_norm_range) in zip(modes, layers, photon_a
     in_preprocessors = []
     in_preprocessors.append(partial(rescale_data, data_range=input_range, scale_data_range=in_norm_range))
     in_preprocessors.append(partial(pad_data, length=2*N))
-
     out_preprocessors = []
-    #out_preprocessors.append(partial(rescale_data, data_range=output_range, scale_data_range=out_norm_range))
-
     postprocessors = []
-    #postprocessors.append(partial(rescale_data, data_range=out_norm_range, scale_data_range=output_range))
 
-    model_name = model_name + "_N" + str(N) + "_L" + str(l) + "_ph" + str(ph_add)# + "_in" + str(in_norm_range) + "_out" + str(out_norm_range)
-    # Build the QNN and train it with the generated dataset
+    model_name = model_name + "_N" + str(N) + "_L" + str(l) + "_ph" + str(ph_add)
+    # Build the QONN and train it with the generated dataset
     qnn, train_loss, valid_loss = build_and_train_model(model_name, N, l, n_inputs, n_outputs, ph_add, is_addition, observable,
                                                         include_initial_squeezing, include_initial_mixing, is_passive_gaussian,
                                                         train_dataset, None, loss_function, basinhopping_iters,
@@ -103,43 +81,16 @@ if is_addition:
     nongauss_op = "â†"
 else:
     nongauss_op = "â"
-legend_labels = [f'N={qnn.N}, L={qnn.layers}, {nongauss_op} in modes {np.array(qnn.ladder_modes) + 1}' for qnn in qnns] # α ∈ {in_norm_range}
+legend_labels = [f'N={qnn.N}, L={qnn.layers}, {nongauss_op} in modes {np.array(qnn.ladder_modes) + 1}' for qnn in qnns]
 
-c=0
-for exp_val_idx in range(start_expval, end_expval):
-    plt.plot(inputs, outputs[:,exp_val_idx], c=colors(c), label=expvals[exp_val_idx])
-    c += 1
-    
-colors = colormaps['tab10']
-markers = ['*', 's', 'v', '+', 'x', '<', '>', 'd', 'p', 'h', '^']
-m = 0
-for (qnn_test_outputs, legend_label) in zip(qnn_outs, legend_labels):
-    c = 0
-    plt.plot([], [],
-             marker=markers[m],
-             linestyle='None',
-             color='black',
-             label=legend_label)
-    for exp_val_idx in range(start_expval, end_expval):
-        plt.plot(inputs, qnn_test_outputs[:,exp_val_idx], marker=markers[m], c=colors(c), alpha=0.25, linestyle='None')
-        c += 1
-    m+=1
-plt.title(f'QONN EVALUATION')
-plt.xlabel('Displacement α')
-plt.xlim()
-plt.ylabel('Statistical moments')
-#plt.ylim(top=output_range[1] + len(qnns)*0.2 + 0.2)
-plt.grid(linestyle='--', linewidth=0.4)
-#plt.legend(loc='upper right')
-plt.legend()
-plt.savefig("figures/result_"+model_name+"_N"+str(modes)+"_L"+str(layers)+"_ph"+str(photon_additions)+"_in"+str(input_range)+".png")
-plt.show()
-plt.clf()
+print('=== MINIMAL LOSSES ACHIEVED ===')
+for i in range(len(legend_labels)):
+    print(f'{legend_labels[i]}: {qnn_loss[i]}')
 
 c=0
 for (train_loss, legend_label) in zip(train_losses, legend_labels):
     x_log = np.log(np.array(range(1,len(train_loss)+1)))
-    plt.plot(np.log(np.array(train_loss)+1), c=colors(c+3%10), label=f'Train loss {legend_label}')#, {input_range} disp')
+    plt.plot(np.log(np.array(train_loss)+1), c=colors(c+3%10), label=f'Train loss {legend_label}')
     c+=1
 
 plt.ylim(bottom=0.0)
@@ -148,8 +99,8 @@ plt.ylabel('Loss value')
 plt.title(f'LOGARITHMIC LOSS')
 plt.grid(linestyle='--', linewidth=0.4)
 plt.legend()
-plt.savefig("figures/logloss_"+model_name+"_N"+str(modes)+"_L"+str(layers)+"_ph"+str(photon_additions)+"_in"+str(input_range)+".png")
 plt.show()
+plt.savefig("figures/logloss_"+model_name+"_N"+str(modes)+"_L"+str(layers)+"_ph"+str(photon_additions)+"_in"+str(input_range)+".pdf")
 plt.clf()
 
 print(f'MINIMAL LOSSES ACHIEVED: {qnn_loss}')
